59c0dd7563d8eb75aaeffdabbdd7ca0457a28192
cglavin50 <cooperglavin@gmail.com>
2025-05-24 13:48:02 -0400

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "My first flake";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-25.05";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ self, nixpkgs, home-manager, ... }: {
+    nixosConfigurations.nixos-btw = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+	# import previous config.nix to keep existing system
+	./configuration.nix
+
+	# make home-manager a module of nixos such that it's deployed automatically on switch
+	home-manager.nixosModules.home-manager
+	{
+	  home-manager.useGlobalPkgs = true;
+	  home-manager.useUserPackages = true;
+
+	  home-manager.users.cooper = import ./home.nix;
+	}
+      ];
+    };
+  };
+}
---
+++ home.nix
@@ -0,0 +1,54 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "cooper";
+  home.homeDirectory = "/home/cooper";
+  home.stateVersion = "25.05"; # current home-manager ver
+  programs.home-manager.enable = true; # Home Manager can install and manage itself
+
+  programs.bash = {
+    enable = true;
+    enableCompletion = true;
+    shellAliases = {
+      ll = "ls -l";
+      btw = "echo i use nixos btw";
+      rebuild = "sudo nixos-rebuild switch --flake ~/dotfiles#nixos-btw";
+      config = "sudo vim /etc/nixos/configuration.nix";
+      homemanager = "sudo vim /etc/nixos/home.nix";
+    };
+  };
+
+  home.packages = with pkgs; [
+    bat
+    fastfetch
+    nnn
+
+    # archives
+    zip
+    xz
+    unzip 
+
+    # networking
+    nmap
+    dnsutils
+    ldns
+
+    # misc utilities
+    which
+    tree
+    file
+    lsof
+    usbutils
+    ethtool
+    sysstat
+
+    # nix
+    nix-output-monitor
+  ];
+
+  programs.git = {
+    enable = true;
+    userName = "cglavin50";
+    userEmail = "cooperglavin@gmail.com";
+  };
+}
